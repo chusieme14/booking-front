8df162cf6396499adf8929a970194352ad07a459
paul <paul.anderson.b.garcia@gmail.com>
2022-05-30 19:43:41 +0800

    update eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [
       {
-        name: 'ThesisFront',
+        name: 'BookingFront',
         exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
+        instances: 1, // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start'
       }
